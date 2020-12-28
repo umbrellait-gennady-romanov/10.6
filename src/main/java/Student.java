d508a98071905ab217ff0926df764042e196137f
gennadyromanov <gennady.romanov@umbrellait.com>
2020-12-28 20:21:00 +0300

    Initial commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,18 @@
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    @Column(name = "registration_date")
+    private Date registrationDate;
+}
